e089ac30440fd82e9dc7f9725d6d7959595b9dfc
Adam Schaefer <saddam.szajbert.i@gmail.com>
2022-12-14 10:46:49 +0100

    new way of menu navigations

---
+++ BloodTypeC.Console/ApplicationNavi.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace BloodTypeC.ConsoleUI
+{
+    class ApplicationNavi
+    {
+        public void Start()
+        {
+            string prompt = "Witaj w Beer-o-pedii! Czy masz skończone 18 lat?";
+            string[] options = { "Tak", "Nie" };
+            MenuLogic startMenu = new MenuLogic(prompt, options);
+            startMenu.DisplayOptions();
+
+            WriteLine("Press any key to exit...");
+            ReadKey(true);
+        }
+    }
+}
---
+++ BloodTypeC.Console/MenuLogic.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console; 
+
+namespace BloodTypeC.ConsoleUI
+{
+    public class MenuLogic
+    {
+        private int SelectedIndex;
+        private string[] Options;
+        private string Prompt;
+
+        public MenuLogic(string prompt, string[] options)
+        {
+            Prompt = prompt;
+            Options = options;
+            SelectedIndex = 0;    
+        }
+
+        public void DisplayOptions()
+        {
+            WriteLine(Prompt);
+            for (int i = 0 ; i < Options.Length; i++) 
+            {
+                string currentOption = Options[i];
+                WriteLine($"<<{currentOption}>>");
+            }
+        }
+    }
+}
