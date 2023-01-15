037d07a6358eb36ea68dab03b482727e15d4fe60
Artur Gromow <artur.gromow@gmail.com>
2023-01-15 20:51:16 +0100

    added DisplayBeer

--- BloodTypeC.Logic/BeerSearch.cs
+++ BloodTypeC.Logic/BeerSearch.cs
@@ -12,31 +12,36 @@ namespace BloodTypeC.Logic
     {
         public static List<Beer> SearchByName(List<Beer> listToSearch, string name) 
         {
-            return listToSearch.Where(beer => beer.Name == name).ToList();
+            return listToSearch.Where(beer => beer.Name.Equals(name,StringComparison.InvariantCultureIgnoreCase)).ToList();
         }
 
         public static List<Beer> SearchByBrewery(List<Beer> listToSearch, string brewery)
         {
-            return listToSearch.Where(beer => beer.Brewery == brewery).ToList();
+            return listToSearch.Where(beer => beer.Brewery.Equals(brewery,StringComparison.InvariantCultureIgnoreCase)).ToList();
         }
 
         public static List<Beer> SearchByFlavor(List<Beer> listToSearch, string searchflavor)
-        {
-            foreach (Beer beer in listToSearch) 
-            {
-                for (int i = 0; i < beer.Flavors.Count; i++)
-                {
-                    beer.Flavors[i] = beer.Flavors[i].ToLower();
-                }
-            }
-
-           return listToSearch.Where(beer => beer.Flavors.Contains(searchflavor.ToLower())).ToList();
+        {          
+           return listToSearch.Where(beer => beer.Flavors.Contains(searchflavor,StringComparer.InvariantCultureIgnoreCase)).ToList();
         }
 
-        public static List<Beer> SearchByAlkVol(List<Beer> listToSearch, double abv)
+        public static List<Beer> SearchByAlkVol(List<Beer> listToSearch, double minAbv,double maxAbv)
         {
+
+            return listToSearch.Where(beer => beer.AlcoholByVolume >= minAbv && beer.AlcoholByVolume <= maxAbv).ToList();
+        }
+        public static void DisplayBeer(List<Beer> listToDisplay) 
+        {
+            if (listToDisplay.Count > 0)
+            {
+                Console.WriteLine($"{"ID".PadRight(10)} | {"Name".PadRight(20)} | {"Brewery".PadRight(20)} | {"Score".PadRight(10)} | {"AlcoholByVolume".PadRight(5)}");
+                foreach (var item in listToDisplay)
+                {
+                    Console.WriteLine($"{item.Id.PadRight(10)} | {item.Name.PadRight(20)} | {item.Brewery.PadRight(20)} | {item.Score.ToString().PadRight(10)} | {item.AlcoholByVolume.ToString().PadRight(5)}");
+                }
+            }
+                Console.WriteLine("Brak piwa o podanej nazwie");
             
-            return listToSearch.Where(beer => beer.AlcoholByVolume >= abv).ToList();
         }
 
     }
