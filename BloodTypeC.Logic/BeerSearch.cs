3e5eb808052914c3edd8fc2da547e943c6eb5f5e
Adam Schaefer <saddam.szajbert.i@gmail.com>
2023-01-19 19:54:29 +0100

    style added to view

--- BloodTypeC.Logic/BeerSearch.cs
+++ BloodTypeC.Logic/BeerSearch.cs
@@ -38,13 +38,13 @@ namespace BloodTypeC.Logic
         {
             if (listToDisplay.Count > 0)
             {
-                int formatLenght = String.Format("|{0,-20}|{1,-30}|{2,-30}|{3,-20}|{4,-20}|{5,-50}|", "ID", "Name", "Brewery", "Score", "AlcoholByVolume", "Beer falvors").Length;
+                int formatLenght = String.Format("|{0,-20}|{1,-30}|{2,-30}|{3,-20}|{4,-20}|{5,-50}|{6,-20}|", "ID", "Name", "Brewery", "Style", "AlcoholByVolume", "Beer falvors" ,"Score").Length;
                 for (int i = 0; i < formatLenght ; i++)
                 {
                     Console.Write("-");
                 }
                 Console.Write("\n");
-                Console.WriteLine(String.Format("|{0,-20}|{1,-30}|{2,-30}|{3,-20}|{4,-20}|{5,-50}|", "ID", "Name", "Brewery", "Score", "AlcoholByVolume", "Beer falvors"));
+                Console.WriteLine(String.Format("|{0,-20}|{1,-30}|{2,-30}|{3,-20}|{4,-20}|{5,-50}|{6,-20}|", "ID", "Name", "Brewery", "Style", "AlcoholByVolume", "Beer falvors", "Score"));
                 for (int i = 0; i < formatLenght; i++)
                 {
                     Console.Write("-");
@@ -54,7 +54,7 @@ namespace BloodTypeC.Logic
                 foreach (var item in listToDisplay)
                 {
                     string beerFalvors = string.Join(",",item.Flavors);
-                    Console.WriteLine(String.Format("|{0,-20}|{1,-30}|{2,-30}|{3,-20}|{4,-20}|{5,-50}|", item.Id, item.Name, item.Brewery, item.Score, item.AlcoholByVolume, beerFalvors));
+                    Console.WriteLine(String.Format("|{0,-20}|{1,-30}|{2,-30}|{3,-20}|{4,-20}|{5,-50}|{6,-20}|", item.Id, item.Name, item.Brewery, item.Style, item.AlcoholByVolume, beerFalvors, item.Score));
                     //Console.WriteLine($"{item.Id.PadRight(10)} | {item.Name.PadRight(20)} | {item.Brewery.PadRight(20)} | {item.Score.ToString().PadRight(10)} | {item.AlcoholByVolume.ToString().PadRight(15)} | {beerFalvors.PadRight(50)}");                
                 }
                 Console.ReadKey();
