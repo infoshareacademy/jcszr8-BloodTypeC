620b17e6ed4ab6018bdddc1e33a37409dbd7f3ca
miniatureluck <105822819+miniatureluck@users.noreply.github.com>
2023-02-21 19:54:31 +0100

    finished favorite list mechanics

--- BloodTypeC.WebApp/Controllers/FavoriteBeersController.cs
+++ BloodTypeC.WebApp/Controllers/FavoriteBeersController.cs
@@ -1,100 +1,124 @@
 ﻿using BloodTypeC.DAL;
+using BloodTypeC.Logic;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Primitives;
 
 namespace BloodTypeC.WebApp.Controllers
 {
     public class FavoriteBeersController : Controller
     {
         // GET: FavoriteBeersController
-        public ActionResult Index()
+        public IActionResult Index()
         {
             return View();
         }
-        public ActionResult AddToFavorites(int id)
+        
+        [HttpGet]
+        public IActionResult AddToFavorites(int id)
         {
-            var beerToFavs = DB.AllBeers.FirstOrDefault(x => x.Id == id.ToString());
-            DB.FavoriteBeers.Add(beerToFavs);
-            return RedirectToAction("AllBeers", "Home");
+            var referer = Request.Headers.Referer.ToString();
+            BeerOperations.AddToFavs(id);
+            if (referer.Contains("Details"))
+            {
+                return RedirectToAction("Details", "Home", new { id });
+            }
+            else
+            {
+                return RedirectToAction("AllBeers", "Home");
+            }
         }
-        public ActionResult RemoveFromFavorites(int id)
+        public IActionResult RemoveFromFavorites(int id)
         {
-            var beerToFavs = DB.AllBeers.FirstOrDefault(x => x.Id == id.ToString());
-            DB.FavoriteBeers.Remove(beerToFavs);
-            return RedirectToAction("Favorites");
+            var referer = Request.Headers.Referer.ToString();
+            BeerOperations.RemoveFromFavs(id);
+            if (referer.Contains("Favorites"))
+            {
+                return RedirectToAction("Favorites");
+            }
+            if (referer.Contains("Details"))
+            {
+                return RedirectToAction("Details", "Home", new { id });
+            }
+            else
+            {
+                return RedirectToAction("AllBeers", "Home");
+            }
         }
-        public ActionResult Favorites()
+        public IActionResult Favorites()
         {
             return View(DB.FavoriteBeers);
         }
 
-        // GET: FavoriteBeersController/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
+        //// GET: FavoriteBeersController/Details/5
+        //public ActionResult Details(int id)
+        //{
+        //    return View();
+        //}
 
-        // GET: FavoriteBeersController/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
+        //// GET: FavoriteBeersController/Create
+        //public ActionResult Create()
+        //{
+        //    return View();
+        //}
 
-        // POST: FavoriteBeersController/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
+        //// POST: FavoriteBeersController/Create
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Create(IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
 
-        // GET: FavoriteBeersController/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
+        //// GET: FavoriteBeersController/Edit/5
+        //public ActionResult Edit(int id)
+        //{
+        //    return View();
+        //}
 
-        // POST: FavoriteBeersController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
+        //// POST: FavoriteBeersController/Edit/5
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Edit(int id, IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
 
-        // GET: FavoriteBeersController/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
+        //// GET: FavoriteBeersController/Delete/5
+        //public ActionResult Delete(int id)
+        //{
+        //    return View();
+        //}
 
-        // POST: FavoriteBeersController/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
+        //// POST: FavoriteBeersController/Delete/5
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Delete(int id, IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
     }
 }
