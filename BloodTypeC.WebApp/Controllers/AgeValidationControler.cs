8405a4818598316c4daf02637cc424d23e5e42e5
Adam Schaefer <saddam.szajbert.i@gmail.com>
2023-02-27 17:42:05 +0100

    pictures/names edited as decided

---
+++ BloodTypeC.WebApp/Controllers/AgeValidationControler.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BloodTypeC.WebApp.Controllers
+{
+    public class AgeValidationControler : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
