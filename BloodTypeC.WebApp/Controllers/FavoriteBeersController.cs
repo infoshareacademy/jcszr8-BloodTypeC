52279842afe3e865c6faa65886c6e4754b0115a9
miniatureluck <105822819+miniatureluck@users.noreply.github.com>
2023-03-17 22:42:45 +0100

    recoded checking view path, removed unused details view from home controller

--- BloodTypeC.WebApp/Controllers/FavoriteBeersController.cs
+++ BloodTypeC.WebApp/Controllers/FavoriteBeersController.cs
@@ -16,59 +16,40 @@ namespace BloodTypeC.WebApp.Controllers
         {
             _favoriteBeersServices = favoriteBeersServices;
         }
-        
+
         [HttpGet]
         public IActionResult AddToFavorites(int id)
         {
             _favoriteBeersServices.AddToFavs(id);
             var referer = GetReferer();
 
-            // Returns the same view from which the method was called
-            // but checks whether it's the Details view that needs the id
-            if (referer.Value.All(ch => char.IsLetter(ch)))
-            {
-                return RedirectToAction(referer.Value, referer.Key);
-            }
-            else
-            {
-                return RedirectToAction("Details", "Beer", new { id });
-            }
+            return RedirectToAction(referer[1], referer[0], new { id });
         }
         public IActionResult RemoveFromFavorites(int id)
         {
             _favoriteBeersServices.RemoveFromFavs(id);
             var referer = GetReferer();
 
-            // Returns the same view from which the method was called
-            // but checks whether it's the Details view that needs the id
+            return RedirectToAction(referer[1], referer[0], new { id });
 
-            if (referer.Value.All(ch => char.IsLetter(ch)))
-            {
-                return RedirectToAction(referer.Value, referer.Key);
-            }
-            else
-            {
-                return RedirectToAction("Details", "Beer", new { id });
-            }
         }
         public IActionResult Favorites()
         {
             return View(_favoriteBeersServices.GetAllFavs());
         }
 
-        public KeyValuePair<string, string> GetReferer()
+        public string[] GetReferer()
         {
             // Extracts the Controller and Action strings from the referer url
-            // Key = Controller
-            // Value = Action
+            // [0] = Controller
+            // [1] = Action
+            // [2] = Id (when available)
 
             var host = Request.Host.ToUriComponent();
             var referer = Request.Headers.Referer.ToString() ?? string.Empty;
             var trimmedUrl = referer.Substring(referer.IndexOf(host) + host.Length + 1);
-            var controller = trimmedUrl.Remove(trimmedUrl.IndexOf('/'));
-            var action = trimmedUrl.Substring(trimmedUrl.LastIndexOf('/') + 1);
-            var controllerAction = new KeyValuePair<string, string>(controller, action);
-            return controllerAction;
+            string[] path = trimmedUrl.Split('/');
+            return path;
         }
     }
 }
