4c8516b44288b966c1ab0aa1d3f930b725392094
Artur Gromow <artur.gromow@gmail.com>
2023-04-11 21:26:09 +0200

    empty flavors in detail view fixed

--- BloodTypeC.WebApp/Controllers/BeerController.cs
+++ BloodTypeC.WebApp/Controllers/BeerController.cs
@@ -32,6 +32,7 @@ namespace BloodTypeC.WebApp.Controllers
         public ActionResult Details(string id)
         {
             var model = _repository.GetById(id);
+            if (model.Flavors.Count() == 0) model.Flavors = new List<string>() { "" };
             var newBeerDto = _mapper.Map<BeerViewModel>(model);
             return View(newBeerDto);
         }
