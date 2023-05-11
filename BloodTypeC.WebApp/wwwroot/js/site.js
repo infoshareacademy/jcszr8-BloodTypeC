41e1b554ccd753771072b2abd1cf195d55ffa177
miniatureluck <105822819+miniatureluck@users.noreply.github.com>
2023-05-11 12:11:55 +0200

    tooltips in create view finished

--- BloodTypeC.WebApp/wwwroot/js/site.js
+++ BloodTypeC.WebApp/wwwroot/js/site.js
@@ -1,3 +1,15 @@
-﻿$(function () {
-    $('[data-toggle="tooltip"]').tooltip({ html: true, boundary: 'window' })
-})
+﻿window.onload = function () {
+    
+    var targets = document.getElementsByClassName("tooltip-button");
+    var tooltips = document.getElementsByClassName("tooltip-text");
+
+
+    for (let i = 0; i < targets.length; i++) {
+        targets[i].addEventListener('mouseover', () => {
+            tooltips[i].style.display = 'block';
+        }, false);
+        targets[i].addEventListener('mouseleave', () => {
+            tooltips[i].style.display = 'none';
+        }, false);
+    }
+}
