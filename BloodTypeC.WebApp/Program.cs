236eb3345480b34b3d125bbfa980fa6f16cd7145
Adam Schaefer <saddam.szajbert.i@gmail.com>
2023-05-19 19:33:13 +0200

    all fixes in styles done

--- BloodTypeC.WebApp/Program.cs
+++ BloodTypeC.WebApp/Program.cs
@@ -15,6 +15,7 @@ namespace BloodTypeC.WebApp
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+                        
                                    
              // Add services to the container.
             builder.Services.AddControllersWithViews();
